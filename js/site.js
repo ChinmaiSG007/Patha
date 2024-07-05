3cacfece5bd4ac707da584091306c42c686dcfda
ChinmaiSG007 <chinmaicsg8787@gmail.com>
2024-07-06 01:11:08 +0530

    Patha first build

---
+++ js/site.js
@@ -0,0 +1,87 @@
+"use strict";
+
+$(document).ready(function () {
+	/* Video Lightbox */
+	if (!!$.prototype.simpleLightboxVideo) {
+		$('.video').simpleLightboxVideo();
+	}
+
+	/*ScrollUp*/
+	if (!!$.prototype.scrollUp) {
+		$.scrollUp();
+	}
+
+	/*Responsive Navigation*/
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-trigger span").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$(this).removeClass("open");
+		} else {
+			$("nav#nav-mobile ul").addClass("expanded").slideDown(250);
+			$(this).addClass("open");
+		}
+	});
+
+	$("#nav-mobile").html($("#nav-main").html());
+	$("#nav-mobile ul a").on("click",function() {
+		if ($("nav#nav-mobile ul").hasClass("expanded")) {
+			$("nav#nav-mobile ul.expanded").removeClass("expanded").slideUp(250);
+			$("#nav-trigger span").removeClass("open");
+		}
+	});
+
+	/* Sticky Navigation */
+	if (!!$.prototype.stickyNavbar) {
+		$('#header').stickyNavbar();
+	}
+
+	$('#content').waypoint(function (direction) {
+		if (direction === 'down') {
+			$('#header').addClass('nav-solid fadeInDown');
+		}
+		else {
+			$('#header').removeClass('nav-solid fadeInDown');
+		}
+	});
+
+});
+
+
+/* Preloader and animations */
+$(window).load(function () { // makes sure the whole site is loaded
+	$('#status').fadeOut(); // will first fade out the loading animation
+	$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+	$('body').delay(350).css({'overflow-y': 'visible'});
+
+	/* WOW Elements */
+	if (typeof WOW == 'function') {
+		new WOW().init();
+	}
+
+	/* Parallax Effects */
+	if (!!$.prototype.enllax) {
+		$(window).enllax();
+	}
+
+});
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+	document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onmouseover = function(event) {
+	if (!event.target.matches('.dropbtn') && !event.target.closest('.dropdown-content')) {
+	  var dropdowns = document.getElementsByClassName("dropdown-content");
+	  var i;
+	  for (i = 0; i < dropdowns.length; i++) {
+		var openDropdown = dropdowns[i];
+		if (openDropdown.classList.contains('show')) {
+		  openDropdown.classList.remove('show');
+		}
+	  }
+	}
+  }
